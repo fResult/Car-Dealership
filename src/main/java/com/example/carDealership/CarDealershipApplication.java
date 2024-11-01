91a6af8c8147f0dddf25670be019822bae8702d0
fResult Sila <styxmaz@gmail.com>
2024-11-01 23:16:46 +0700

    update `application.yml` file

--- src/main/java/com/example/carDealership/CarDealershipApplication.java
+++ src/main/java/com/example/carDealership/CarDealershipApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarDealershipApplication {
 	public static void main(String[] args) {
-        System.out.println("Test");
 		SpringApplication.run(CarDealershipApplication.class, args);
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO CAR_COLLECTION (latitude, longitude, id, schedule_time, car_model, color, contact_name, contact_phone_number, reference, status, vehicle_registration_id) VALUES (10, 20, 2, CURRENT_TIMESTAMP, 'Prius', 'Silver', 'Chris', '0879999999', 'REF1', 'Scheduled', 'KC2323');
-INSERT INTO CAR_COLLECTION (latitude, longitude, id, schedule_time, car_model, color, contact_name, contact_phone_number, reference, status, vehicle_registration_id) VALUES (10, 30, 3, CURRENT_TIMESTAMP, 'Altis', 'Black', 'Chris', '0879999999', 'REF2', 'Scheduled', 'KC2324');
+INSERT INTO car_collection (latitude, longitude, id, schedule_time, car_model, color, contact_name, contact_phone_number, reference, status, vehicle_registration_id) VALUES (10, 20, 2, CURRENT_TIMESTAMP, 'Prius', 'Silver', 'Chris', '0879999999', 'REF1', 'Scheduled', 'KC2323');
+INSERT INTO car_collection (latitude, longitude, id, schedule_time, car_model, color, contact_name, contact_phone_number, reference, status, vehicle_registration_id) VALUES (10, 30, 3, CURRENT_TIMESTAMP, 'Altis', 'Black', 'Chris', '0879999999', 'REF2', 'Scheduled', 'KC2324');
 
-INSERT INTO STOCK (id, model, quantity) VALUES (1, 'Prius', 1);
-INSERT INTO STOCK (id, model, quantity) VALUES (2, 'Altis', 1);
+INSERT INTO stock (id, model, quantity) VALUES (1, 'Prius', 1);
+INSERT INTO stock (id, model, quantity) VALUES (2, 'Altis', 1);
