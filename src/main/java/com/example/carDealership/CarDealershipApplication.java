eff27e4af7043aee9f6ffe5ae82b2bfafa545c18
fResult Sila <styxmaz@gmail.com>
2024-11-03 16:16:18 +0700

    test schedule CarCollection

--- src/main/java/com/example/carDealership/CarDealershipApplication.java
+++ src/main/java/com/example/carDealership/CarDealershipApplication.java
@@ -1,8 +1,15 @@
 package com.example.carDealership;
 
 import com.example.carDealership.domain.EventBus;
+import com.example.carDealership.domain.carCollecting.entities.CarCollection;
+import com.example.carDealership.domain.carCollecting.valueObjects.CarColor;
+import com.example.carDealership.domain.carCollecting.valueObjects.Coordinate;
+import com.example.carDealership.domain.carCollecting.valueObjects.VehicleRegistrationId;
+import com.example.carDealership.domain.validation.ValidationException.ValidattionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class CarDealershipApplication {
@@ -12,5 +19,30 @@ public class CarDealershipApplication {
         var bus = context.getBean(EventBus.class);
         var warehouseEventHandler = context.getBean(com.example.carDealership.domain.warehouse.EventHandler.class);
         bus.subscribe(warehouseEventHandler);
+
+        testScheduleCarCollection();
+    }
+
+    public static void testScheduleCarCollection() {
+        var registrationId = new VehicleRegistrationId("SS-SS");
+        var pickupPlace = new Coordinate(90, 180);
+
+        try {
+            var carCollection = CarCollection.schedule(
+                    "Korn",
+                    new Date(),
+                    "Uncle Kile",
+                    "0999999999",
+                    "StapleX street",
+                    "Civic Type R",
+                    CarColor.Black,
+                    registrationId,
+                    pickupPlace
+            );
+
+            System.out.println("Scheduled Car Collection Time: " + carCollection.getScheduleTime());
+        } catch (ValidattionException ex) {
+            ex.printStackTrace();
+        }
     }
 }
