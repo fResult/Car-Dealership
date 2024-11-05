0ac0af3072a52f46f1fce9b199fb45e9d704b736
fResult Sila <styxmaz@gmail.com>
2024-11-05 19:41:12 +0700

    update return types of `CarCollectingRepository`'s methods

--- src/main/java/com/example/carDealership/domains/CarCollectingRepository.java
+++ src/main/java/com/example/carDealership/domains/CarCollectingRepository.java
@@ -3,10 +3,13 @@ package com.example.carDealership.domains;
 import com.example.carDealership.domains.carCollecting.entities.CarCollection;
 import com.example.carDealership.domains.warehouse.Stock;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CarCollectingRepository {
     CarCollection saveCarCollection(CarCollection entity);
     Stock saveStock(Stock entity);
 
-    CarCollection findCarCollectionById(long id);
-    Stock findStockByModel(String model);
+    Optional<CarCollection> findCarCollectionById(long id);
+    List<Stock> findStocksByModel(String model);
 }
