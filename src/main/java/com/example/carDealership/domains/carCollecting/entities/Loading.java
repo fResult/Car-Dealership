4e023c66dc8719668aa5ccac909fe95ab206ae92
fResult Sila <styxmaz@gmail.com>
2024-11-05 19:43:32 +0700

    create `droppedCarAtWarehouse` in the `Loading` entity

--- src/main/java/com/example/carDealership/domains/carCollecting/entities/Loading.java
+++ src/main/java/com/example/carDealership/domains/carCollecting/entities/Loading.java
@@ -1,7 +1,6 @@
 package com.example.carDealership.domains.carCollecting.entities;
 
 import com.example.carDealership.domains.CarCollectingRepository;
-import com.example.carDealership.domains.CarCollectingRepository.CarCollectingRepository;
 import com.example.carDealership.domains.validations.ValidattionException;
 import com.example.carDealership.domains.warehouse.Stock;
 
@@ -9,6 +8,7 @@ import java.util.Date;
 
 // From the Loading form for car dropping at warehouse
 public class Loading {
+    private final CarCollectingRepository carCollectingRepository;
     private CarCollection relatedCarCollection;
     private Stock relatedStock;
 
@@ -17,8 +17,8 @@ public class Loading {
     private Date timestamp;
     private String comment;
 
-    public Loading(CarCollectingRepository carCollectionRepository) {
-
+    public Loading(CarCollectingRepository carCollectingRepository) {
+        this.carCollectingRepository = carCollectingRepository;
     }
 
     public long getId() {
@@ -38,13 +38,20 @@ public class Loading {
     }
 
     // And other related fields in the form
-    public void droppedCarAtWarehouse() throws ValidattionException {
+    public CarCollection droppedCarAtWarehouse(long carCollectionId) throws ValidattionException {
         // Do something with its own entity
         // Such as validation, update things, etc.
+
+        // TODO: Handle throwing error in the proper place
+        var carCollection = carCollectingRepository.findCarCollectionById(carCollectionId).orElseThrow();
+        var stock = carCollectingRepository.findStocksByModel(carCollection.getCarModel()).getFirst();
 
         relatedCarCollection.carDroppedToWarehouse();
         relatedStock.incrementStockQuantity(1);
 
+        carCollectingRepository.saveStock(stock);
+        carCollection = carCollectingRepository.saveCarCollection(carCollection);
 
+        return carCollection;
     }
 }
