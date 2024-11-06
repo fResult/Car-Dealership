826debe506055529211bfb1ce11582ef0e4e3bd0
fResult <styxmaz@gmail.com>
2024-11-07 03:40:10 +0700

    add a remark for `EventBus.publish` method

--- src/main/java/com/example/carDealership/domains/EventBus.java
+++ src/main/java/com/example/carDealership/domains/EventBus.java
@@ -1,6 +1,7 @@
 package com.example.carDealership.domains;
 
 public interface EventBus {
+    // NOTE: We can receive `metadata` as XML, JSON, GRPC, Protobuf, Binary or other
     void publish(DomainEvent event, String metadata);
     void subscribe(EventBusSubscriber subscriber);
 }
