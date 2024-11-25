a1cc1efb07135852c216eec215bb67cb2b73afaa
fResult <styxmaz@gmail.com>
2024-11-26 05:37:47 +0700

    Update gradle config and upgrade some dependencies.
    - Upgrade `org.hibernate.orm` plugin from \'6.5.3.Final\' to \'6.6.3.Final\'
    - Configure args of `-Dnet.bytebuddy.experimental=true` when start spring boot by `gradle bootRun`

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   application
   id("org.springframework.boot") version "3.4.0"
   id("io.spring.dependency-management") version "1.1.6"
-  id("org.hibernate.orm") version "6.5.3.Final"
+  id("org.hibernate.orm") version "6.6.3.Final"
 //  id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
@@ -31,7 +31,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+//  implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
 
   runtimeOnly("com.h2database:h2")
 
@@ -47,11 +47,11 @@ hibernate {
 
 tasks.withType<Test> {
   useJUnitPlatform()
-//  jvmArgs = listOf("-Dnet.bytebuddy.experimental=true")
+  // jvmArgs = listOf("-Dnet.bytebuddy.experimental=true")
 }
 
-//tasks.withType<BootRun> {
-//    doFirst {
-//        jvmArgs = listOf("-Dnet.bytebuddy.experimental=true")
-//    }
-//}
+tasks.withType<BootRun> {
+  doFirst {
+    jvmArgs = listOf("-Dnet.bytebuddy.experimental=true")
+  }
+}
